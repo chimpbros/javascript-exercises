d488705f70252db46985e6e11654e3df670f4c8c
chimpbros <kevinnue00@gmail.com>
2022-12-13 15:09:32 +0700

    Complete removeFromArrayExercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    // iterate over array using filter method
+    // return array that is not args item
+    const removeItem = array.filter(item => {
+        return !args.includes(item);
+    });
+    return removeItem;
 };
 
 // Do not edit below this line
