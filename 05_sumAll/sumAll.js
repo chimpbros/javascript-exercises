67ad19604a1fdd203ec81ef9f1b5cae6dee610c4
chimpbros <kevinnue00@gmail.com>
2022-12-14 13:09:43 +0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumCount = 0;
+    let start;
+    let end;
+    if(num1 < num2){
+        start = num1;
+        end = num2;
+    }
+    else if (num2 < num1){
+        start = num2;
+        end = num1;
+    }
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if(typeof(num1) != "number" || typeof(num2) != "number"){
+        return "ERROR"
+    }
+    for(let i = start; i <= end; i++){
+        sumCount+= i;
+    }
+    return sumCount;
 };
 
 // Do not edit below this line
