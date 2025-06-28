a9d9f8e025a7cf507da88ecd0bbbb9b280680259
chimpbros <kevinnue@gmail.com>
2025-06-28 22:06:05 +0700

    Implement removeFromArray, sumAll, leapYears, and tempConversion functions
    - Add removeFromArray function to remove specified values from array
    - Implement sumAll to calculate sum of integers in a range with error handling
    - Create leapYears function to determine if a year is a leap year
    - Complete tempConversion functions for Fahrenheit/Celsius conversion
    - Enable all previously skipped tests for these exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    // convert theArgs to array
+    // loop through arr
+    // for each item in arr loop through argsArr
+    // if arr[i] in argsArr, remove the item
+    // return arr
+    let argsArr = Array.from(theArgs);
+    for(let i = 0; i < arr.length; i++){
+        if(argsArr.includes(arr[i])){
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    if(first < 0 || last < 0){
+        return "ERROR";
+    }
+    else if(!(Number.isInteger(first) && Number.isInteger(last))){
+        return "ERROR";
+    }
+    else if(first < last){
+            for (let i = first; i <= last; i++){
+                sum += i;
+            }
+            return sum;
+        }
+    else{
+            for (let i = last; i <= first; i++){
+                    sum += i;
+                }
+                return sum;
+        }
 };
 
 // Do not edit below this line
