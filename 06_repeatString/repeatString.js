baef9c3498c65235f2ef1d8ab62dec1be31d7569
chimpbros <kevinnue00@gmail.com>
2025-06-28 14:32:42 +0700

    Complete repeatString and reverseString exercises
    Implement the `repeatString` function, including error handling for negative repeat counts.
    Implement the `reverseString` function.
    Enable all skipped tests for both exercises, confirming their correct functionality.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if(times < 0){
+        return "ERROR";
+    }
+    else {
+        let outString = "";
+        for(let i = 0; i < times; i++){
+            outString += str;
+        }
+    return outString;
+    }
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseArr = str.split("").reverse();
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
